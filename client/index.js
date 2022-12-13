ab1f9e85d9bc452caaa3cc407fad6abcf7b7516a
aditiakusuma <aditiakusuma800@gmail.com>
2022-12-13 21:25:20 +0700

    remove unused

--- client/index.js
+++ client/index.js
@@ -1,16 +1,3 @@
 document.addEventListener("DOMContentLoaded", () => {
-  var splide = new Splide(".splide");
-  splide.mount();
-
-  lightGallery(document.getElementById("gallery"), {
-    plugins: [lgZoom, lgThumbnail],
-  });
-
-  const customClub = document.getElementById("fClubCustom")
-  document.getElementById("fClub").addEventListener('change', (event) => {
-    console.log(event.target.value)
-    customClub.style.display = event.target.value === "Lainnya" ? "block" : "none"
-  });
-
-  getData();
+  
 });
