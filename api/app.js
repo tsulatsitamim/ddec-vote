1bae5a25731951bc8f270eee07ddbd4381314fdb
aditiakusuma <aditiakusuma800@gmail.com>
2022-12-14 22:35:30 +0700

    add validate + error

--- api/app.js
+++ api/app.js
@@ -1,4 +1,6 @@
 require("dotenv").config();
+const { body, validationResult } = require("express-validator");
+
 const express = require("express");
 const app = express();
 const port = 3000;
@@ -34,58 +36,75 @@ app.use(
 
 app.get("/result", async (req, res) => {
   try {
-    const votes = await knex("votes").select('candidate')
-    .count("candidate", { as: "count" })
-    .groupBy("candidate")
-    .as("counts");
+    const votes = await knex("votes")
+      .select("candidate")
+      .count("candidate", { as: "count" })
+      .groupBy("candidate")
+      .as("counts");
 
-    return res.send(votes.reduce((obj, item) => Object.assign(obj, { [item.candidate]: item.count }), {}));
+    return res.send(
+      votes.reduce(
+        (obj, item) => Object.assign(obj, { [item.candidate]: item.count }),
+        {}
+      )
+    );
   } catch (error) {
     console.log(error);
   }
 });
 
-app.post("/votes", async (req, res) => {
-  try {
-    const name = req.body.name;
-    const phone = req.body.phone;
-    const email = req.body.email;
-    const candidate = req.body.candidate;
-
-    const date = new Date().toISOString().substring(0, 10);
-
-    const haveVoteToday = await knex("votes")
-      .where('date', date)
-      .where((q) => q.where("email", email).orWhere("phone", phone))
-      .first();
-
-    if (haveVoteToday) {
-      return res
-        .status(400)
-        .send({ message: "Vote Minimal 1 kali per email/nomor HP" });
+app.post(
+  "/votes",
+  body("name").notEmpty().withMessage('Nama Harus Diisi'),
+  body("phone").notEmpty().withMessage('Nomor Telepon Harus Diisi').isMobilePhone().withMessage('Nomor Telepon tidak sesuai'),
+  body("email").notEmpty().withMessage('Email Harus Diisi').isEmail().withMessage('Email tidak sesuai'),
+  body('candidate').notEmpty().withMessage('Kandidat Harus Diisi').isIn(['indonesia', 'malaysia']).withMessage('Kandidat hanya bisa di isi dengan malaysia atai indonesia'),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
+    try {
+      const name = req.body.name;
+      const phone = req.body.phone;
+      const email = req.body.email;
+      const candidate = req.body.candidate;
 
-    const id = await knex("votes").insert({
-      name,
-      phone,
-      email,
-      candidate,
-      date
-    });
+      const date = new Date().toISOString().substring(0, 10);
 
-    res.json({
-      id: id[0],
-      name,
-      phone,
-      email,
-      candidate,
-      date
-    });
-  } catch (e) {
-    console.log(e);
-    // next(e)
+      const haveVoteToday = await knex("votes")
+        .where("date", date)
+        .where((q) => q.where("email", email).orWhere("phone", phone))
+        .first();
+
+      if (haveVoteToday) {
+        return res
+          .status(400)
+          .send({ message: "Vote Minimal 1 kali per email/nomor HP" });
+      }
+
+      const id = await knex("votes").insert({
+        name,
+        phone,
+        email,
+        candidate,
+        date,
+      });
+
+      res.json({
+        id: id[0],
+        name,
+        phone,
+        email,
+        candidate,
+        date,
+      });
+    } catch (e) {
+      console.log(e);
+      // next(e)
+    }
   }
-});
+);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
