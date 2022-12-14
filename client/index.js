5a9b91516b106522e50dd71e79e89377c7be8df2
aditiakusuma <aditiakusuma800@gmail.com>
2022-12-14 20:27:36 +0700

    show score and time

--- client/index.js
+++ client/index.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-  
+  const el = document.getElementById("time-counter");
+
+  function time() {
+    el.textContent = dayjs().format('D MMM - HH:mm:ss');
+  }
+
+  setInterval(time, 1000);
 });
